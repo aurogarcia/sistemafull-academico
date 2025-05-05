51ff50a48d4e1d28d89305d27c071a92fde9c8f1
aurogarcia <aurogarcia833@gmail.com>
2025-05-05 20:15:05 -0300

    deu certo

--- sistema-academico/src/app/app.component.ts
+++ sistema-academico/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'sistema-academico';
-}
+ import { Router, RouterOutlet } from '@angular/router';
+ import { CommonModule } from '@angular/common';
+ @Component({
+   selector: 'app-root',
+   standalone: true,
+   imports: [CommonModule, RouterOutlet],
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
+ })
+ export class AppComponent {
+   constructor(private router: Router) {}
+ 
+   irParaLista() {
+     this.router.navigate(['/']);
+   }
+   irParaNovo() {
+     this.router.navigate(['/novo']);
+   }
+ }
--- sistema-academico/src/app/app.routes.ts
+++ sistema-academico/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+ import { AlunoFormComponent } from './aluno/aluno-form/aluno-form.component';
+ import { AlunoListComponent } from './aluno/aluno-list/aluno-list.component';
+ 
+ 
+ export const routes: Routes = [
+  { path: '', component: AlunoListComponent },
+  { path: 'novo', component: AlunoFormComponent }
+ ];
